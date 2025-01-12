fa9fbb80032230753676ef489fca09ffd735ab98
Dustin K Greco <dkgreco@thetroveofgems.tech>
2025-01-12 14:47:26 -0700

    Remove debug

--- server/config/security/enable.app.security.js
+++ server/config/security/enable.app.security.js
@@ -29,7 +29,6 @@ export const enableApplicationSecurity = (app) => {
             imgSrc: [
                 "'self'",
                 'https://res.cloudinary.com',
-                'https://www.photo-album.thetroveofgems.tech'
             ]
         },
     };
