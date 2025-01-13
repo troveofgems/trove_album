143fde82c5bd25e47264c676c1bf1bf3b95afb7f
Dustin K Greco <dkgreco@thetroveofgems.tech>
2025-01-13 15:12:27 -0700

    Fixed Mobile Navigation

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -42,7 +42,6 @@ export const NavBar = ({ onViewChange }) => {
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
                         <NavDropdown title="Change Gallery" id="navbarScrollingDropdown">
