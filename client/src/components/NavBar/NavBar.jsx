f0eba0feae1ce5ce34ab87a3e20544e36bfb9ad7
Dustin K Greco <dkgreco@thetroveofgems.tech>
2025-01-13 15:18:04 -0700

    Add style back for nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -42,6 +42,7 @@ export const NavBar = ({ onViewChange }) => {
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100%' }}
                         navbarScroll
                     >
                         <NavDropdown title="Change Gallery" id="navbarScrollingDropdown">
